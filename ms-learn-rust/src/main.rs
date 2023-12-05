21581d02368e45c51a4bcc5289a977e383f79034
edicon <hslee.edicon@gmail.com>
2023-12-05 10:21:52 +0900

    rust: shadow variable

--- ms-learn-rust/src/main.rs
+++ ms-learn-rust/src/main.rs
@@ -6,4 +6,17 @@ fn main() {
 
     println!("The Number is {}", a_num);
     println!("The word is {}", a_word);
+
+    shadow_variable();
+}
+
+// shadow: mutable 선언 없이 변수를 변경
+fn shadow_variable() {
+    let shadow_num = 5;
+    let shadow_num = shadow_num + 1;
+    let shadow_num = shadow_num * 2;
+
+    // shadow_num = 10; // error: cannot assign twice to immutable variable
+
+    println!("The value of shadow_num is: {}", shadow_num);
 }
