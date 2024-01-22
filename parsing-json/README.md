79cec94a1128dd06a6763620662aea673259357d
edicon <hslee.edicon@gmail.com>
2024-01-22 16:35:01 +0900

    parsing_json: serde function

--- parsing-json/README.md
+++ parsing-json/README.md
@@ -1,1 +1,2 @@
 [Parsing JSON in Rust](https://www.shuttle.rs/blog/2024/01/18/parsing-json-rust)
+- read serde for helping by Ctrl-]
--- parsing-json/src/main.rs
+++ parsing-json/src/main.rs
@@ -1,12 +1,21 @@
 use serde_json::{Result, Value};
+use serde_json::json;
+use serde::{Deserialize, Serialize}; // install with features = ["derive"] for using derive macros
+
+// Parsing JSON using serde!
+#[derive(Serialize, Deserialize, Debug)]
+pub struct MyStruct {
+    message: String,
+}
 
 fn main() {
     println!("Parsing JSON using serde!");
     untyped_example().unwrap();
+    convert_json_to_struct();
 }
 
 fn untyped_example() -> Result<()> {
-    let data = r#"
+    let json_string = r#"
         {
             "name": "John Doe",
             "age": 43,
@@ -17,10 +26,24 @@ fn untyped_example() -> Result<()> {
         }"#;
 
     // Parse the steing of fdata into serde_json::Value
-    let v: Value = serde_json::from_str(data)?;
+    let v: Value = serde_json::from_str(json_string)?;
 
     // Access parts of the data by indexing with square bracket's
     println!("Please call {} at the number {}", v["name"], v["phones"][0]);
 
     Ok(())
+}
+
+fn convert_json_to_struct() {
+    let literal_json_string = r#"
+        {
+            "message": "Hello, World!"
+        }"#;
+    let raw_json_string = &json!({ "message": "Hello, World!" }).to_string();
+    // let raw_json_string = json!({ "message": "Hello, World!" });
+
+    let my_struct:  MyStruct = serde_json::from_str(literal_json_string).unwrap();
+    let my_struct2: MyStruct = serde_json::from_str(raw_json_string).unwrap();
+    println!("My struct: {:?}", my_struct);
+    println!("My struct2: {:?}", my_struct2);
 }
